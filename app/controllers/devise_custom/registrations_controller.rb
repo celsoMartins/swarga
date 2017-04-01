a670b643603226e90e572006c45f5e0c06e6f134
Celso Martins <celso.av.martins@gmail.com>
2017-04-01 02:26:08 -0300

    removed useless code

--- app/controllers/devise_custom/registrations_controller.rb
+++ app/controllers/devise_custom/registrations_controller.rb
@@ -7,9 +7,5 @@ module DeviseCustom
     def sign_up_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
-
-    def account_update_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-    end
   end
 end
