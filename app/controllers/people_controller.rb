49f5b70cd9911b0a2bcb788a93c468cc31ccbd09
Celso Martins <celso.av.martins@gmail.com>
2017-12-24 15:39:39 -0200

    update person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,15 +2,26 @@
 
 class PeopleController < AuthenticatedController
   before_action :find_camping_group
+  before_action :find_person, only: %i[edit update]
 
   def new
-    @new_person = Person.new(camping_group: @camping_group)
+    @person = Person.new(camping_group: @camping_group)
   end
 
   def create
-    @new_person = Person.new(person_params.merge(camping_group: @camping_group))
-    return redirect_to camping_group_path(@camping_group) if @new_person.save
+    @person = Person.new(person_params.merge(camping_group: @camping_group))
+    return redirect_to camping_group_path(@camping_group) if @person.save
     render :new
+  end
+
+  def edit; end
+
+  def update
+    if @person.update(person_params)
+      redirect_to camping_group_path(@camping_group, notice: t('people.update.success'))
+    else
+      render :edit
+    end
   end
 
   private
@@ -21,5 +32,9 @@ class PeopleController < AuthenticatedController
 
   def find_camping_group
     @camping_group ||= CampingGroup.find(params[:camping_group_id])
+  end
+
+  def find_person
+    @person = @camping_group.people.find(params[:id])
   end
 end
--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe PeopleController, type: :controller do
       before { post :create, params: { camping_group_id: 'foo' } }
       it { expect(response).to redirect_to new_user_session_path }
     end
+    describe 'GET #edit' do
+      before { get :edit, params: { camping_group_id: 'foo', id: 'bar' } }
+      it { expect(response).to redirect_to new_user_session_path }
+    end
+    describe 'PUT #update' do
+      before { put :update, params: { camping_group_id: 'foo', id: 'bar' } }
+      it { expect(response).to redirect_to new_user_session_path }
+    end
   end
 
   context 'authenticated' do
@@ -22,8 +30,8 @@ RSpec.describe PeopleController, type: :controller do
       context 'with a valid camping group' do
         it 'instatiates the instance variable and renders the template' do
           get :new, params: { camping_group_id: camping_group }
-          expect(assigns(:new_person)).to be_a_new Person
-          expect(assigns(:new_person).camping_group).to eq camping_group
+          expect(assigns(:person)).to be_a_new Person
+          expect(assigns(:person).camping_group).to eq camping_group
           expect(response).to render_template :new
         end
       end
@@ -46,16 +54,72 @@ RSpec.describe PeopleController, type: :controller do
         end
       end
       context 'with invalid parameters' do
-        context 'and passing a valid camping group' do
+        context 'and passing a valid camping group and invalid person' do
           it 'does not create the camping group and re-render the form with errors' do
             post :create, params: { camping_group_id: camping_group, person: { foo: 'bar' } }
             expect(response).to render_template :new
             expect(Person.last).to be_nil
-            expect(assigns(:new_person).errors.full_messages).to eq ['Nome não pode ficar em branco']
+            expect(assigns(:person).errors.full_messages).to eq ['Nome não pode ficar em branco']
           end
         end
         context 'and passing an invalid camping group' do
           before { post :create, params: { camping_group_id: 'foo' } }
+          it { expect(response).to be_not_found }
+        end
+      end
+    end
+    describe 'GET #edit' do
+      let(:person) { Fabricate :person, camping_group: camping_group }
+      context 'with a valid camping group' do
+        context 'and a valid person' do
+          it 'finds the person and renders the template' do
+            get :edit, params: { camping_group_id: camping_group, id: person }
+            expect(assigns(:person)).to eq person
+            expect(assigns(:person).camping_group).to eq camping_group
+            expect(response).to render_template :edit
+          end
+        end
+        context 'and an invalid person' do
+          before { get :edit, params: { id: 'foo', camping_group_id: camping_group } }
+          it { expect(response).to be_not_found }
+        end
+      end
+      context 'with an invalid camping group' do
+        before { get :edit, params: { id: person, camping_group_id: 'foo' } }
+        it { expect(response).to be_not_found }
+      end
+    end
+
+    describe 'PUT #update' do
+      let(:person) { Fabricate :person, camping_group: camping_group }
+
+      context 'with valid parameters' do
+        context 'and a valid person' do
+          it 'updates the camping group as reserved and redirects to index' do
+            put :update, params: { camping_group_id: camping_group, id: person, person: { full_name: 'foo', document_number: '3423', phone: '444' } }
+            expect(response).to redirect_to camping_group_path(camping_group, notice: I18n.t('people.update.success'))
+            updated_person = Person.last
+            expect(updated_person.camping_group).to eq camping_group
+            expect(updated_person.full_name).to eq 'foo'
+            expect(updated_person.document_number).to eq '3423'
+            expect(updated_person.phone).to eq '444'
+          end
+        end
+      end
+      context 'with invalid parameters' do
+        context 'and passing a valid camping group and invalid person' do
+          it 'does not update the camping group and re-render the form with errors' do
+            put :update, params: { camping_group_id: camping_group, id: person, person: { full_name: nil } }
+            expect(response).to render_template :edit
+            expect(assigns(:person).errors.full_messages).to eq ['Nome não pode ficar em branco']
+          end
+          context 'and an invalid person' do
+            before { get :edit, params: { id: 'foo', camping_group_id: camping_group } }
+            it { expect(response).to be_not_found }
+          end
+        end
+        context 'and passing an invalid camping group' do
+          before { put :update, params: { camping_group_id: 'foo', id: person } }
           it { expect(response).to be_not_found }
         end
       end
