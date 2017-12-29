37ff168746007709638ed9012578826cf9f79dea
Celso Martins <celso.av.martins@gmail.com>
2017-12-29 02:33:03 -0200

    updated code climate test coverage

--- db/structure.sql
+++ db/structure.sql
@@ -1,13 +1,5 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 10.1
--- Dumped by pg_dump version 10.1
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
